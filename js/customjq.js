1b54f93a0087fefdf143783dcf96bb1fd6624a86
Steven Walker-Roberts <avswalker@gmail.com>
2017-06-20 23:11:33 +0000

    initial commit

---
+++ js/customjq.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+	
+	var scrollState = 0;
+
+	/* after 150px, created fixed top menu */
+	$(window).on("scroll", function(e) {
+		
+	  if ($(this).scrollTop() > 300 && scrollState == 0) {
+		$(".fix").addClass("fix-nav");
+		$('.navbar').animate({
+			opacity: '1.0'
+		});
+		$('#name').removeClass('hidden');
+		$('#xsbrand').removeClass('hidden');
+		scrollState = 1;
+		console.log("fixing nav");
+	  } else if ($(this).scrollTop() <= 300 && scrollState == 1) {
+		$(".fix").removeClass("fix-nav");
+		$('.navbar').animate({
+			opacity: '0.7'
+		});
+		$('#name').addClass('hidden');
+		$('#xsbrand').addClass('hidden');
+		scrollState = 0;
+		console.log("remove fix");
+	  }
+	  
+	});
+	
+});
